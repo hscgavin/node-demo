a4dcb26efa9ba39a18d844672408dbb86ee84e37
Gavin He <hscgavin@gmail.com>
2017-09-29 21:06:34 +1000

    Add router and dummy response

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const router = require('./router')
 const app = express()
 
 
 app.use(morgan('combined'))
 app.use(bodyParser.json({type: '*/*'}))
+
+router(app)
 
 const port = process.env.PORT || 3090
 app.listen(port)
